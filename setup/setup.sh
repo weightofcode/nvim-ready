6c219983944de975887096f75c311a1a3b4388c9
weightofcode <czorile@gmail.com>
2026-03-22 21:28:01 +0200

    Neovim install on *nix systems

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,54 @@
 #!/usr/bin/env bash
 set -e
-echo "NVIM_CONFIG_INFO: Running Linux/macOS setup..."
+echo "NVIM_CONFIG_INFO: Running Unix setup..."
+
+# package manager >>>>>>>>
+if command -v apt >/dev/null 2>&1; then
+    PKG_MANAGER = "apt"
+elif command -v pacman >/dev/null 2>&1; then
+    PKG_MANAGER = "pacman"
+elif command -v dnf >/dev/null 2>&1; then
+    PKG_MANAGER = "dnf"
+elif command -v brew >/dev/null 2>&1; then
+    PKG_MANAGER = "brew"
+elif command -v pkg >/dev/null 2>&1; then
+    PKG_MANAGER = "pkg"
+else
+    echo "NVIM_SETUP_ERROR: No supported package manager found."
+    exit 1
+fi
+echo "NVIM_SETUP_ERROR: Detected package manager: $PKG_MANAGER"
+# <<<<<<<< package manager
+
+# install Neovim >>>>>>>>
+install_package() {
+    pkg_name = $1
+    case $PKG_MANAGER in 
+        apt)
+            sudo apt update
+            sudp apt install -y "$pkg_name"
+            ;;
+        pacman)
+            sudo pacman -Sy --noconfirm "$pkg_name"
+            ;;
+        dnf)
+            sudo dnf install -y "$pkg_name"
+            ;;
+        brew)
+            brew install "$pkg_name"
+            ;;
+        pkg)
+            sudo pkg install -y "$pkg_name"
+            ;;
+    esac
+}
+
+if ! command -v nvim >/dev/null 2>&1; then
+    echo "NVIM_SETUP_INFO: Installing Neovim..."
+    install_package neovim
+else
+    echo "NVIM_SETUP_INFO: Neovim already installed."
+fi
+
+echo "NVIM_SETUP_INFO: Neovim already installed."
+# <<<<<<<< install Neovim
