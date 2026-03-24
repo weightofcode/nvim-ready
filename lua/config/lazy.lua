230bd583dcc71a52ef4b21784d6c0dc791f99c64
weightofcode <czorile@gmail.com>
2026-03-24 18:53:28 +0200

    replace kanagawa with zenbones

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,8 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "kanagawa" } },
+  -- install = { colorscheme = { "kanagawa" } },
+  install = { colorscheme = { "zenbones" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -6,7 +6,7 @@ return {
             transparent=true,   -- not working in Win11 Terminal
         });
         -- vim.cmd("KanagawaCompile"); -- compile already enabled on line 5
-        vim.cmd("colorscheme kanagawa");
+        -- vim.cmd("colorscheme kanagawa");
     end,
     -- build = function()
     --     vim.cmd("KanagawaCompile");
