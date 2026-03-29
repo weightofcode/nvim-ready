c48439ca8ff1eaa2b5defdc758275b8c0b1c5b91
weightofcode <czorile@gmail.com>
2026-03-29 20:25:12 +0300

    initial commmit for LSP

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,118 @@
+return {
+    -- For description of the code below, check Kickstart Nvim on GitHub
+    -- As this is a copy-paste from there (and adapted)
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        {
+            'mason-org/mason.nvim',
+            opts = {},
+        },
+        'mason-org/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+        { 'j-hui/fidget.nvim', opts = {} },
+    },
+    config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+            callback = function(event)
+                local map = function(keys, func, desc, mode)
+                    mode = mode or 'n'
+                    vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                end
+
+                map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+                map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+                map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+                    local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                        buffer = event.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+                    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                        buffer = event.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                    vim.api.nvim_create_autocmd('LspDetach', {
+                        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                        callback = function(event2)
+                            vim.lsp.buf.clear_references()
+                            vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                        end,
+                    })
+                end
+            end,
+        })
+
+        -- Enable the following language servers
+        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+        --  See `:help lsp-config` for information about keys and how to configure
+        -- @type table<string, vim.lsp.Config>
+        local servers = {
+            -- clangd = {},
+            -- gopls = {},
+            pyright = {},
+            -- rust_analyzer = {},
+            --
+            -- Some languages (like typescript) have entire language plugins that can be useful:
+            --    https://github.com/pmizio/typescript-tools.nvim
+            --
+            -- But for many setups, the LSP (`ts_ls`) will work just fine
+            -- ts_ls = {},
+
+            stylua = {}, -- Used to format Lua code
+
+            -- Special Lua Config, as recommended by neovim help docs
+            lua_ls = {
+                on_init = function(client)
+                    if client.workspace_folders then
+                        local path = client.workspace_folders[1].name
+                        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+                    end
+
+                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                        runtime = {
+                            version = 'LuaJIT',
+                            path = { 'lua/?.lua', 'lua/?/init.lua' },
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+                            --  See https://github.com/neovim/nvim-lspconfig/issues/3189
+                            library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+                                '${3rd}/luv/library',
+                                '${3rd}/busted/library',
+                            }),
+                        },
+                    })
+                end,
+                settings = {
+                    Lua = {},
+                },
+            },
+        }
+
+        -- Ensure the servers and tools above are installed
+        --
+        -- To check the current status of installed tools and/or manually install
+        -- other tools, you can run
+        --    :Mason
+        --
+        -- You can press `g?` for help in this menu.
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, {
+            -- You can add other tools here that you want Mason to install
+        })
+
+        require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+        for name, server in pairs(servers) do
+            vim.lsp.config(name, server)
+            vim.lsp.enable(name)
+        end
+    end,
+}
